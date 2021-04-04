91bff9ed9b7e548c1694275f06adb35b67c930fa
fokse <fokseee@gmail.com>
2021-04-05 00:40:35 +0300

    Update main.js

--- phaek_modules/site/main.js
+++ phaek_modules/site/main.js
@@ -1,5 +1,5 @@
 import { createPreviewItems } from './modules/create-preview-items.js';
-import { initKolonkiSwitcher } from './modules/kolonki-switcher.js';
+import { initKolonkiSwitcher } from 'https://raw.githubusercontent.com/fokse/Amods/main/phaek_modules/site/modules/kolonki-switcher.js';
 import { BLACK_STANDS } from './modules/constants.js';
 import { previewList } from './modules/elements.js';
 
